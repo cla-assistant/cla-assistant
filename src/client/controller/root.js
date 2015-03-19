e8c914e352997416e47668c1d1de04db8a946058
Anton <anton.kharitonov@sap.com>
2015-03-19 12:40:36 +0100

    try to improve log in process

--- src/client/controller/root.js
+++ src/client/controller/root.js
@@ -2,59 +2,28 @@
 // Root Controller
 // *****************************************************
 
-module.controller('RootCtrl', ['$rootScope', '$scope', '$stateParams', '$HUB', '$RPC', '$RAW',
-    function($rootScope, $scope, $stateParams, $HUB, $RPC, $RAW) {
+module.controller('RootCtrl', ['$rootScope', '$scope', '$stateParams', '$HUB', '$RPC', '$RAW', '$timeout',
+    function($rootScope, $scope, $stateParams, $HUB, $RPC, $RAW, $timeout) {
 
         $rootScope.user = {value: {admin: false}};
+        $rootScope.loading = true;
 
         $HUB.call('user', 'get', {}, function(err, res){
+            $rootScope.loading = false;
             if (err) {
                 return;
             }
 
-            $rootScope.user = res;
-            $rootScope.user.value.admin = false;
+            $timeout(function() {
+                $rootScope.user = res;
+                $rootScope.user.value.admin = false;
 
-            if (res.meta.scopes.indexOf('write:repo_hook') > -1) {
-                $rootScope.user.value.admin = true;
-            }
+                if (res.meta.scopes.indexOf('write:repo_hook') > -1) {
+                    $rootScope.user.value.admin = true;
+                }
 
-            $rootScope.$broadcast('user');
+                $rootScope.$broadcast('user');
+            }, 0);
         });
-
-
-        // $rootScope.$on('repos:get', function(event, repo) {
-        //     $scope.repo = repo;
-        // });
-
-        // $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, error) {
-
-        //     if( !($stateParams.user && $stateParams.repo) ) {
-        //         $scope.hook = {};
-        //     }
-
-        //     if( $stateParams.user && $stateParams.repo &&
-        //         $scope.repo && $scope.repo.permissions.admin &&
-        //         toParams.user !== fromParams.user ) {
-
-        //         $scope.hook = $RPC.call('webhook', 'get', {
-        //             user: $stateParams.user,
-        //             repo: $stateParams.repo
-        //         });
-        //     }
-        // });
-
-        // $scope.createWebhook = function() {
-        //     $scope.creating = $RPC.call('webhook', 'create', {
-        //         user: $stateParams.user,
-        //         repo: $stateParams.repo,
-        //         user_uuid: $rootScope.user.value.id
-        //     }, function(err, hook) {
-        //         if(!err) {
-        //             $scope.hook = hook;
-        //             $scope.created = true;
-        //         }
-        //     });
-        // };
     }
 ]);
