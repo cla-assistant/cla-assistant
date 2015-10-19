2cb561e356b51afb7b61d449529a6497a52d9c01
Anton <anton.kharitonov@sap.com>
2015-10-19 11:19:47 +0200

    remove old code showing cla diffs

--- src/client/controller/cla.js
+++ src/client/controller/cla.js
@@ -10,7 +10,6 @@ module.controller( 'ClaController', ['$window', '$scope', '$stateParams', '$RAW'
 
 				$scope.cla = null;
 				$scope.signed = false;
-				$scope.signedCLA = null;
 				$scope.repoExists = false;
 				$scope.params = $stateParams;
 				$scope.user = {};
@@ -61,42 +60,6 @@ module.controller( 'ClaController', ['$window', '$scope', '$stateParams', '$RAW'
 								}
 						});
 				};
-
-
-				// function getDiff(){
-				//     return $RPCService.call('cla', 'get', {
-				//         repo: $stateParams.repo,
-				//         owner: $stateParams.user,
-				//         gist: {
-				//             gist_url: $scope.signedCLA.gist_url,
-				//             gist_version: $scope.signedCLA.gist_version
-				//         }
-				//     }, function(err, cla) {
-				//         if(!err) {
-				//             $scope.signedCLA.text = cla.value.raw;
-				//         }
-				//     });
-				// }
-
-				function getLastSignature(){
-						var promise = $RPCService.call('cla', 'getLastSignature', {
-								repo: $stateParams.repo,
-								owner: $stateParams.user
-						});
-
-						promise.then(function(data){
-								$RPCService.call('cla', 'get', {
-										repo: $stateParams.repo,
-										owner: $stateParams.user,
-										gist: {gist_url: data.value.gist_url,
-														gist_version: data.value.gist_version}
-								}).then(function(cla_data){
-										$scope.signedCLA.text = cla_data.value.raw;
-								});
-						});
-
-						return promise;
-				}
 
 				$scope.agree = function(){
 						var acceptUrl = '/accept/' + $stateParams.user + '/' + $stateParams.repo;
