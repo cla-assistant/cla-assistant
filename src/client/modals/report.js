cb21f1277257b12196cebb229cddc73c934c6c9d
Anton <anton.kharitonov@sap.com>
2015-11-06 16:05:16 +0100

    ui for gist version selection

--- src/client/modals/report.js
+++ src/client/modals/report.js
@@ -1,8 +1,11 @@
 module.controller('ReportCtrl', function($scope, $modalInstance, $window, repo) {
 	$scope.claRepo = repo;
-	$scope.gist = null;
-
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
+	// $scope.gist = null;
+	$scope.selectedVersion = {};
+	$scope.selectedVersion.version = $scope.gist.history[0];
+	
+	console.log($scope.selectedVersion);
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
 });
