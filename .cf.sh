49457a9b25d7d3c27b46f84d39bb9c510ff1845d
Anton <anton.kharitonov@sap.com>
2016-06-29 14:42:43 +0200

    build v1.1 on separate CF instance

--- .cf.sh
+++ .cf.sh
@@ -3,7 +3,12 @@
 # Download cf command line client
 wget -O - https://cli.run.pivotal.io/stable\?release\=linux64-binary\&source\=github | tar xvz -C .
 
-./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
+if [$1 = "cla-assistant-feature"]
+    then
+    ./cf login -a https://api.run.pivotal.io -u $CF2_USER -p $CF2_PASS
+    else
+    ./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
+fi
 
 echo $1
 
