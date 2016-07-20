13e9e31659be9a03fddb7479d864469a3633098d
Anton <anton.kharitonov@sap.com>
2016-07-20 11:06:43 +0200

    show All Versions on report only once

--- src/client/modals/report.js
+++ src/client/modals/report.js
@@ -1,12 +1,10 @@
 module.controller('ReportCtrl', function($scope, $modalInstance, $window, item) {
 	$scope.claItem = item;
 	$scope.newContributors = {loading: false};
-	$scope.history = $scope.gist.history;
+	$scope.history = $scope.gist.history.concat({text: 'All versions'});
 	// $scope.gist = null;
 	$scope.selectedVersion = {};
 	$scope.selectedVersion.version = $scope.gist.history[0];
-
-	$scope.history.push({text: 'All versions'});
 
 	$scope.cancel = function () {
 		$modalInstance.dismiss('cancel');
