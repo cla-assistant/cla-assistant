f1aee7404cbf80e5605845e8370d029d409560a3
Anton <anton.kharitonov@sap.com>
2016-07-22 10:29:00 +0200

    fix login for users with public scope

--- src/server/controller/user.js
+++ src/server/controller/user.js
@@ -10,21 +10,24 @@ var scope;
 
 function checkReturnTo(req, res, next) {
     scope = null;
+    if (!req.session) {
+        req.session = {};
+    }
 
     if (req.query.public  === 'true') {
         scope = config.server.github.user_scope.concat();
+        req.session.requiredScope = 'public';
     }
     if (req.query.admin === 'true') {
         scope = config.server.github.admin_scope.concat();
+        req.session.requiredScope = 'admin';
     }
     if (req.query.org_admin === 'true') {
         scope.push('admin:org_hook');
+        req.session.requiredScope = 'org_admin';
     }
     var returnTo = req.query.public === 'true' ?  req.session.next : '/';
     if (returnTo) {
-        if (!req.session) {
-            req.session = {};
-        }
         req.session.returnTo = returnTo;
     }
     passport.authenticate('github', {scope: scope})(req, res, next);
@@ -35,7 +38,7 @@ router.get('/auth/github', checkReturnTo);
 // router.get('/auth/github/callback', passport.authenticate('github', { successReturnToOrRedirect: '/', failureRedirect: '/' }));
 router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
     function (req, res) {
-        if(req.user && !req.query.public && ( !req.user.scope || req.user.scope.indexOf('write:repo_hook') < 0 )) {
+        if(req.user && req.session.requiredScope != 'public' && ( !req.user.scope || req.user.scope.indexOf('write:repo_hook') < 0 )) {
             return res.redirect('/auth/github?admin=true');
         }
         res.redirect(req.session.returnTo || '/');
