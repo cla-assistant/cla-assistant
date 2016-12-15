313fd92d71bdf58c309612cd0521d5a05fbe9199
Anton <anton.kharitonov@sap.com>
2016-12-15 16:31:13 +0100

    add args to the error when no committers found

--- src/server/services/cla.js
+++ src/server/services/cla.js
@@ -11,10 +11,10 @@ var orgService = require('../services/org');
 var repoService = require('../services/repo');
 var config = require('../../config');
 
-module.exports = function () {
+module.exports = function() {
     var claService;
 
-    var getGistObject = function (gist_url, gist_version, token) {
+    var getGistObject = function(gist_url, gist_version, token) {
         var deferred = q.defer();
         try {
             var gistArray = gist_url.split('/'); // https://gist.github.com/KharitonOff/60e9b5d7ce65ca474c29
@@ -40,11 +40,11 @@ module.exports = function () {
             }
         };
 
-        req = https.request(options, function (res) {
-            res.on('data', function (chunk) {
+        req = https.request(options, function(res) {
+            res.on('data', function(chunk) {
                 data += chunk;
             });
-            res.on('end', function () {
+            res.on('end', function() {
                 try {
                     data = JSON.parse(data);
                 } catch (e) {
@@ -55,29 +55,29 @@ module.exports = function () {
         });
 
         req.end();
-        req.on('error', function (e) {
+        req.on('error', function(e) {
             deferred.reject(e);
         });
         return deferred.promise;
     };
 
 
-    var checkAll = function (users, args) {
+    var checkAll = function(users, args) {
         var deferred = q.defer();
         var all_signed = true;
         var promises = [];
         var user_map = { signed: [], not_signed: [], unknown: [] };
         if (!users) {
-            deferred.reject('There are no users to check :( ');
+            deferred.reject('There are no users to check :( ', args);
             return deferred.promise;
         }
-        users.forEach(function (user) {
+        users.forEach(function(user) {
             args.user = user.name;
             user_map.not_signed.push(user.name);
             if (!user.id) {
                 user_map.unknown.push(user.name);
             }
-            promises.push(claService.get(args, function (err, cla) {
+            promises.push(claService.get(args, function(err, cla) {
                 if (err) {
                     logger.warn(new Error(err).stack);
                 }
@@ -92,54 +92,53 @@ module.exports = function () {
                 }
             }));
         });
-        q.all(promises).then(function () {
+        q.all(promises).then(function() {
             deferred.resolve({ signed: all_signed, user_map: user_map });
         });
         return deferred.promise;
     };
 
-    var check = function (repo, owner, gist_url, user, pr_number, token, repoId, orgId) {
+    var check = function(repo, owner, gist_url, user, pr_number, token, repoId, orgId) {
         var deferred = q.defer();
 
-        getGistObject(gist_url, undefined, token).then(function (gist) {
-            if (!gist.history) {
-                deferred.reject('No versions found for the given gist url');
-                return;
-            }
+        getGistObject(gist_url, undefined, token).then(function(gist) {
+                if (!gist.history) {
+                    deferred.reject('No versions found for the given gist url');
+                    return;
+                }
 
-            var args = {
-                user: user,
-                gist: gist_url,
-                gist_version: gist.history[0].version,
-                repo: repo,
-                owner: owner,
-            };
-            args.repoId = repoId ? repoId : undefined;
-            args.orgId = orgId ? orgId : undefined;
+                var args = {
+                    user: user,
+                    gist: gist_url,
+                    gist_version: gist.history[0].version,
+                    repo: repo,
+                    owner: owner,
+                };
+                args.repoId = repoId ? repoId : undefined;
+                args.orgId = orgId ? orgId : undefined;
 
-            if (user) {
-                claService.get(args, function (error, cla) {
-                    deferred.resolve({ signed: !!cla });
-                });
-            }
-            else if (pr_number) {
-                args.number = pr_number;
-                repoService.getPRCommitters(args, function (error, committers) {
-                    if (error) {
-                        logger.warn(new Error(error).stack);
-                    }
-                    checkAll(committers, args).then(
-                        function (result) {
-                            deferred.resolve(result);
-                        },
-                        function (error_msg) {
-                            deferred.reject(error_msg);
+                if (user) {
+                    claService.get(args, function(error, cla) {
+                        deferred.resolve({ signed: !!cla });
+                    });
+                } else if (pr_number) {
+                    args.number = pr_number;
+                    repoService.getPRCommitters(args, function(error, committers) {
+                        if (error) {
+                            logger.warn(new Error(error).stack);
                         }
-                    );
-                });
-            }
-        },
-            function (e) {
+                        checkAll(committers, args).then(
+                            function(result) {
+                                deferred.resolve(result);
+                            },
+                            function(error_msg) {
+                                deferred.reject(error_msg);
+                            }
+                        );
+                    });
+                }
+            },
+            function(e) {
                 deferred.reject(e);
             }
         );
@@ -161,9 +160,9 @@ module.exports = function () {
     //     return deferred.promise;
     // };
 
-    var getRepo = function (args, done) {
+    var getRepo = function(args, done) {
         var deferred = q.defer();
-        repoService.get(args, function (err, repo) {
+        repoService.get(args, function(err, repo) {
             if (!err && repo) {
                 deferred.resolve(repo);
             } else {
@@ -176,12 +175,12 @@ module.exports = function () {
         return deferred.promise;
     };
 
-    var getLinkedItem = function (repo, owner, token) {
+    var getLinkedItem = function(repo, owner, token) {
         var deferred = q.defer();
         token = token || config.server.github.token;
 
         if (owner && !repo) {
-            orgService.get({ org: owner }, function (err, linkedOrg) {
+            orgService.get({ org: owner }, function(err, linkedOrg) {
                 if (linkedOrg) {
                     deferred.resolve(linkedOrg);
                 } else {
@@ -189,14 +188,14 @@ module.exports = function () {
                 }
             });
         } else {
-            repoService.getGHRepo({ owner: owner, repo: repo, token: token }, function (e, ghRepo) {
-                if(e){
+            repoService.getGHRepo({ owner: owner, repo: repo, token: token }, function(e, ghRepo) {
+                if (e) {
                     // could not find the GH Repo
                     deferred.reject(e);
                 } else {
-                    orgService.get({ orgId: ghRepo.owner.id }, function (err, linkedOrg) {
+                    orgService.get({ orgId: ghRepo.owner.id }, function(err, linkedOrg) {
                         if (!linkedOrg) {
-                            repoService.get({ repoId: ghRepo.id }, function (error, linkedRepo) {
+                            repoService.get({ repoId: ghRepo.id }, function(error, linkedRepo) {
                                 if (linkedRepo) {
                                     deferred.resolve(linkedRepo);
                                 } else {
@@ -214,23 +213,23 @@ module.exports = function () {
     };
 
     claService = {
-        getGist: function (args, done) {
+        getGist: function(args, done) {
             var gist_url = args.gist ? args.gist.gist_url || args.gist.url || args.gist : undefined;
             var gist_version = args.gist ? args.gist.gist_version : undefined;
 
-            getGistObject(gist_url, gist_version, args.token).then(function (gistObj) {
+            getGistObject(gist_url, gist_version, args.token).then(function(gistObj) {
                 done(null, gistObj);
-            }, function (err) {
+            }, function(err) {
                 done(err);
             });
         },
 
-        get: function (args, done) {
+        get: function(args, done) {
             var deferred = q.defer();
             var query = { user: args.user, gist_url: args.gist, gist_version: args.gist_version, org_cla: false };
 
-            var findCla = function () {
-                CLA.findOne(query, function (err, cla) {
+            var findCla = function() {
+                CLA.findOne(query, function(err, cla) {
                     deferred.resolve();
                     if (typeof done === 'function') {
                         done(err, cla);
@@ -238,7 +237,7 @@ module.exports = function () {
                 });
             };
             if (!args.repoId && !args.orgId) {
-                getRepo(args, function (error, repo) {
+                getRepo(args, function(error, repo) {
                     if (error || !repo) {
                         deferred.reject();
                         if (typeof done === 'function') {
@@ -260,9 +259,9 @@ module.exports = function () {
         },
 
         //Get last signature of the user for given repository and gist url
-        getLastSignature: function (args, done) {
+        getLastSignature: function(args, done) {
             var deferred = q.defer();
-            getLinkedItem(args.repo, args.owner, args.token).then(function (item) {
+            getLinkedItem(args.repo, args.owner, args.token).then(function(item) {
                 var query = { user: args.user, gist_url: item.gist };
                 if (item.orgId) {
                     query.ownerId = item.orgId;
@@ -272,7 +271,7 @@ module.exports = function () {
                 }
 
                 // CLA.findOne(query, { 'repo': '*', 'owner': '*', 'created_at': '*', 'gist_url': '*', 'gist_version': '*', 'user': '*', 'custom_fields': '*' }, { select: { 'created_at': -1 } }, function (err, cla) {
-                CLA.findOne({ '$query': query, '$orderby': { 'created_at': -1 } }, function (err, cla) {
+                CLA.findOne({ '$query': query, '$orderby': { 'created_at': -1 } }, function(err, cla) {
                     if (!err && cla) {
                         deferred.resolve(cla);
                     }
@@ -280,7 +279,7 @@ module.exports = function () {
                         done(err, cla);
                     }
                 });
-            }, function (err) {
+            }, function(err) {
                 deferred.reject(err);
                 if (typeof done === 'function') {
                     done(err);
@@ -290,9 +289,9 @@ module.exports = function () {
         },
 
 
-        check: function (args, done) {
+        check: function(args, done) {
             if (!args.gist || !args.token) {
-                getLinkedItem(args.repo, args.owner, args.token).then( function (item) {
+                getLinkedItem(args.repo, args.owner, args.token).then(function(item) {
                     args.gist = item.gist;
                     if (item.orgId) {
                         args.orgId = item.orgId;
@@ -300,42 +299,42 @@ module.exports = function () {
                         args.repoId = item.repoId;
                     }
 
-                    check(args.repo, args.owner, args.gist, args.user, args.number, item.token, args.repoId, args.orgId).then(function (result) {
+                    check(args.repo, args.owner, args.gist, args.user, args.number, item.token, args.repoId, args.orgId).then(function(result) {
                         done(null, result.signed, result.user_map);
-                    }, function (err) {
+                    }, function(err) {
                         done(err);
                     });
 
-                }, function (e) {
+                }, function(e) {
                     done(e);
                 });
             } else {
-                check(args.repo, args.owner, args.gist, args.user, args.number, args.token, args.repoId, args.orgId).then(function (result) {
+                check(args.repo, args.owner, args.gist, args.user, args.number, args.token, args.repoId, args.orgId).then(function(result) {
                     done(null, result.signed, result.user_map);
-                }, function (err) {
+                }, function(err) {
                     done(err);
                 });
             }
         },
 
-        sign: function (args, done) {
+        sign: function(args, done) {
             var self = this;
             var org, repo;
 
-            getLinkedItem(args.repo, args.owner, args.token).then(function (item) {
+            getLinkedItem(args.repo, args.owner, args.token).then(function(item) {
                 org = item.orgId ? item : undefined;
                 repo = item.orgId ? undefined : item;
 
                 var argsToCheck = args;
                 argsToCheck.orgId = item.orgId ? item.orgId : undefined;
 
-                self.check(argsToCheck, function (e, signed) {
+                self.check(argsToCheck, function(e, signed) {
                     if (e || signed) {
                         done(e);
                         return;
                     }
 
-                    getGistObject(item.gist, undefined, item.token).then(function (gist) {
+                    getGistObject(item.gist, undefined, item.token).then(function(gist) {
                         var argsToCreate = {};
                         argsToCreate.gist = repo ? repo.gist : org.gist;
                         argsToCreate.gist_version = gist.history[0].version;
@@ -348,7 +347,7 @@ module.exports = function () {
                         argsToCreate.userId = args.userId;
                         argsToCreate.custom_fields = args.custom_fields;
 
-                        self.create(argsToCreate, function (error) {
+                        self.create(argsToCreate, function(error) {
                             if (error) {
                                 done(error);
                                 return;
@@ -361,14 +360,14 @@ module.exports = function () {
         },
 
         //Get list of signed CLAs for all repos the user has contributed to
-        getSignedCLA: function (args, done) {
+        getSignedCLA: function(args, done) {
             var selector = [];
-            var findCla = function (query, repoList, claList, cb) {
-                CLA.find(query, { 'repo': '*', 'owner': '*', 'created_at': '*', 'gist_url': '*', 'gist_version': '*' }, { sort: { 'created_at': -1 } }, function (err, clas) {
+            var findCla = function(query, repoList, claList, cb) {
+                CLA.find(query, { 'repo': '*', 'owner': '*', 'created_at': '*', 'gist_url': '*', 'gist_version': '*' }, { sort: { 'created_at': -1 } }, function(err, clas) {
                     if (err) {
                         logger.warn(new Error(err).stack);
                     } else {
-                        clas.forEach(function (cla) {
+                        clas.forEach(function(cla) {
                             if (repoList.indexOf(cla.repo) < 0) {
                                 repoList.push(cla.repo);
                                 claList.push(cla);
@@ -379,11 +378,11 @@ module.exports = function () {
                 });
             };
 
-            repoService.all(function (e, repos) {
+            repoService.all(function(e, repos) {
                 if (e) {
                     logger.warn(new Error(e).stack);
                 }
-                repos.forEach(function (repo) {
+                repos.forEach(function(repo) {
                     selector.push({
                         user: args.user,
                         repo: repo.repo,
@@ -392,8 +391,8 @@ module.exports = function () {
                 });
                 var repoList = [];
                 var uniqueClaList = [];
-                findCla({ $or: selector }, repoList, uniqueClaList, function () {
-                    findCla({ user: args.user }, repoList, uniqueClaList, function () {
+                findCla({ $or: selector }, repoList, uniqueClaList, function() {
+                    findCla({ user: args.user }, repoList, uniqueClaList, function() {
                         done(null, uniqueClaList);
                     });
                 });
@@ -405,11 +404,12 @@ module.exports = function () {
         // repo (mandatory)
         // owner (mandatory)
         // token (optional)
-        getLinkedItem: function (args, done) {
+        getLinkedItem: function(args, done) {
             getLinkedItem(args.repo, args.owner, args.token).then(
-                function (item) {
+                function(item) {
                     done(null, item);
-                }, function (err) {
+                },
+                function(err) {
                     done(err);
                 }
             );
@@ -454,7 +454,7 @@ module.exports = function () {
         //	orgId (mandatory) and
         //	gist.gist_url (mandatory)
         //	gist.gist_version (optional)
-        getAll: function (args, done) {
+        getAll: function(args, done) {
             if (!args.gist || !args.gist.gist_url || (!args.repoId && !args.orgId)) {
                 done('Wrong arguments, gist url or repo id are missing');
                 return;
@@ -473,7 +473,7 @@ module.exports = function () {
             CLA.find(selection, done);
         },
 
-        create: function (args, done) {
+        create: function(args, done) {
             var now = new Date();
 
             CLA.create({
@@ -488,10 +488,10 @@ module.exports = function () {
                 created_at: now,
                 org_cla: args.org_cla,
                 custom_fields: args.custom_fields
-            }, function (err, res) {
+            }, function(err, res) {
                 done(err, res);
             });
         }
     };
     return claService;
-} ();
+}();
