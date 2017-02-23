25bcf746d963c383a7a267ba2334149121daaa18
Anton <anton.kharitonov@sap.com>
2017-02-23 15:38:55 +0100

    log pr source

--- src/server/webhooks/pull_request.js
+++ src/server/webhooks/pull_request.js
@@ -44,9 +44,8 @@ function handleWebHook(args) {
 }
 
 module.exports = function (req, res) {
-    log.debug(req.args.action);
     if (req.args.pull_request && req.args.pull_request.html_url) {
-        log.info('pull request ' + req.args.action + ' ' + req.args.pull_request.html_url);
+        console.log('pull request ' + req.args.action + ' ' + req.args.pull_request.html_url);
     }
     if (['opened', 'reopened', 'synchronize'].indexOf(req.args.action) > -1) {
         var args = {
