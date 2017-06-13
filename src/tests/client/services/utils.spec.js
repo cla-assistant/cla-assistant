3284e512f12c639a192e4b7e0c80a3cd2d2f0e19
Anton Kharitonov <anton.kharitonov@sap.com>
2017-06-13 11:27:04 +0200

    remove unused variables

--- src/tests/client/services/utils.spec.js
+++ src/tests/client/services/utils.spec.js
@@ -26,40 +26,38 @@ describe('Utils service', function () {
                 }
             },
             'html_url': 'https://gist.github.com/aa5a315d61ae9438b18d',
-            'history': [
-                {
-                    'url': 'https://api.github.com/gists/aa5a315d61ae9438b18d/57a7f021a713b1c5a6a199b54cc514735d2d462f',
-                    'version': '57a7f021a713b1c5a6a199b54cc514735d2d462f',
-                    'user': {
-                        'login': 'octocat',
-                        'id': 1
-                    },
-                    'committed_at': '2010-04-14T02:15:15Z'
-                }
-            ]
+            'history': [{
+                'url': 'https://api.github.com/gists/aa5a315d61ae9438b18d/57a7f021a713b1c5a6a199b54cc514735d2d462f',
+                'version': '57a7f021a713b1c5a6a199b54cc514735d2d462f',
+                'user': {
+                    'login': 'octocat',
+                    'id': 1
+                },
+                'committed_at': '2010-04-14T02:15:15Z'
+            }]
         };
     }));
 
-    it('should return name of the given gist', function(){
+    it('should return name of the given gist', function () {
         var name = utils.getGistAttribute(testData.gist, 'filename');
 
         name.should.be.equal('Ring');
     });
 
-    it('should return file name of the given gist', function(){
+    it('should return file name of the given gist', function () {
         testData.gist.files['ring.erl'].filename = undefined;
         var name = utils.getGistAttribute(testData.gist, 'fileName');
 
         name.should.be.equal('ring.erl');
     });
 
-    it('should return "updated_at" of the given gist', function(){
+    it('should return "updated_at" of the given gist', function () {
         var name = utils.getGistAttribute(testData.gist, 'updated_at');
 
         name.should.be.equal('2010-04-14T02:15:15Z');
     });
-    
-    it('should not fail if gist obj has no files', function(){
+
+    it('should not fail if gist obj has no files', function () {
         testData.gist.files = undefined;
 
         var name = utils.getGistAttribute(testData.gist, 'filename');
