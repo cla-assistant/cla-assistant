07d008dd8717258dc9629b33c54bb39e16353a8c
Anton Kharitonov <anton.kharitonov@sap.com>
2018-06-29 10:42:27 +0200

    linked items can be modified now

---
+++ src/client/modals/editLinkedItem.js
@@ -0,0 +1,44 @@
+module.controller('EditLinkedItemCtrl', function ($scope, $modalInstance, $window, $modal, linkItemService, item, gist, gists) {
+    $scope.linkedItem = item;
+    $scope.gists = gists;
+    $scope.selected = {};
+    $scope.errorMsg = [];
+
+    $scope.selected.item = angular.copy(item);
+
+    function initGist() {
+        $scope.selected.gist = gists.find(function (g) {
+            return g.url === gist.html_url;
+        }) || gist;
+    }
+
+    $scope.clear = function ($event) {
+        $event.stopPropagation();
+        $scope.selected.gist = undefined;
+    };
+
+    $scope.gistShareInfo = function () {
+        $modal.open({
+            templateUrl: '/modals/templates/info_share_gist.html',
+            controller: 'InfoCtrl',
+            windowClass: 'howto'
+        });
+    };
+    $scope.ok = function () {
+        $scope.selected.item.gist = $scope.selected.gist.url;
+        linkItemService.updateLink($scope.selected.item).then(function success(data) {
+            if (data.value) {
+                $modalInstance.close(data.value);
+            }
+        }, function failure(err) {
+            $scope.errorMsg.push(err);
+        });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    initGist();
+
+});
---
+++ src/client/services/linkItem.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.factory('linkItemService', ['$RPCService',
+    function ($RPCService) {
+        function createNewItem(item, options, type) {
+            if (!type) {
+                type = options;
+                options = item;
+            }
+            var newItem = {
+                gist: options.gist.url || options.gist,
+                sharedGist: options.sharedGist,
+                whiteListPattern: options.whiteListPattern,
+                minFileChanges: options.minFileChanges,
+                minCodeChanges: options.minCodeChanges
+            };
+            if (type === 'repo') {
+                newItem.repoId = item.repoId || item.id;
+                newItem.repo = item.repo || item.name;
+                newItem.owner = item.owner.login || item.owner;
+            } else {
+                newItem.orgId = item.orgId || item.id;
+                newItem.org = item.org || item.login;
+                newItem.excludePattern = options.excludePattern;
+            }
+
+            return newItem;
+        }
+
+        return {
+            createLink: function (item, options) {
+                var type = item.full_name ? 'repo' : 'org';
+                var newItem = createNewItem(item, options, type);
+
+                return $RPCService.call(type, 'create', newItem);
+            },
+
+            updateLink: function (item) {
+                var type = item.repoId ? 'repo' : 'org';
+                var newItem = createNewItem(item, type);
+
+                return $RPCService.call(type, 'update', newItem);
+            }
+        };
+    }
+]);
