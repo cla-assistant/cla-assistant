94583011d08a96ea448100eb84f8e36c2657dd5a
Michael Tseng <miczeng@microsoft.com>
2019-04-22 15:23:39 -0700

    Enable CCLA feature in CLA-Assistant

--- src/config.js
+++ src/config.js
@@ -109,7 +109,7 @@ module.exports = {
         feature_flag: {
             pre_populate_user_pull_request: process.env.PRE_POPULATE_USER_PULL_REQUEST === 'true',
             required_signees: process.env.REQUIRED_SIGNEES || '',
-            organization_override_enabled: process.env.ORG_OVERRIDE_ENABLED || false,
+            organization_override_enabled: process.env.ORG_OVERRIDE_ENABLED === 'true' || false,
             close_comment: process.env.CLOSE_COMMENT === 'true',
             enable_private_repos: process.env.ENABLE_PRIVATE_REPOS === 'true',
             always_show_unsigned_list: process.env.ALWAYS_SHOW_UNSIGNED_LIST === 'true',
