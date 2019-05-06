c936949a15bcbb70f53ec9e57bea2b2b90e5caec
Anton Kharitonov <anton.kharitonov@sap.com>
2019-05-06 18:55:03 +0200

    refactoring: replace callback functions with promises in services/org.js

--- src/server/services/org.js
+++ src/server/services/org.js
@@ -1,16 +1,16 @@
 require('../documents/org');
-let mongoose = require('mongoose');
-let Org = mongoose.model('Org');
+const mongoose = require('mongoose');
+const Org = mongoose.model('Org');
 
-let selection = function (args) {
-    let selectArguments = args.orgId ? { orgId: args.orgId } : { org: args.org };
+const selection = function (args) {
+    const selectArguments = args.orgId ? { orgId: args.orgId } : { org: args.org };
 
     return selectArguments;
 };
 
-module.exports = {
-    create: function (args, done) {
-        Org.create({
+class OrgService {
+    async create(args) {
+        return Org.create({
             orgId: args.orgId,
             org: args.org,
             gist: args.gist,
@@ -22,50 +22,40 @@ module.exports = {
             whiteListPattern: args.whiteListPattern,
             privacyPolicy: args.privacyPolicy,
             updatedAt: new Date()
-        }, function (err, org) {
-            done(err, org);
-        });
-    },
-
-    update: function (args, done) {
-        this.get(args, function (err, org) {
-            if (err) {
-                done(err);
-
-                return;
-            }
-            org.gist = args.gist;
-            org.token = args.token ? args.token : org.token;
-            org.sharedGist = !!args.sharedGist;
-            org.excludePattern = args.excludePattern;
-            org.minFileChanges = args.minFileChanges;
-            org.minCodeChanges = args.minCodeChanges;
-            org.whiteListPattern = args.whiteListPattern;
-            org.privacyPolicy = args.privacyPolicy;
-            org.updatedAt = new Date();
-
-            org.save(done);
-        });
-    },
-
-    get: function (args, done) {
-        Org.findOne(selection(args), function (err, org) {
-            if (!err && !org) {
-                err = 'Organization not found in Database';
-            }
-            done(err, org);
-        });
-    },
-
-    getMultiple: function (args, done) {
-        Org.find({ orgId: { $in: args.orgId } }, done);
-    },
-
-    getOrgWithSharedGist: function (gist, done) {
-        Org.find({ gist: gist, sharedGist: true }, done);
-    },
-
-    remove: function (args, done) {
-        Org.findOneAndRemove(selection(args), done);
+        })
     }
-};
+
+    async get(args) {
+        return Org.findOne(selection(args))
+    }
+
+    async update(args) {
+        const org = await this.get(args)
+        org.gist = args.gist
+        org.token = args.token ? args.token : org.token
+        org.sharedGist = !!args.sharedGist
+        org.excludePattern = args.excludePattern
+        org.minFileChanges = args.minFileChanges
+        org.minCodeChanges = args.minCodeChanges
+        org.whiteListPattern = args.whiteListPattern
+        org.privacyPolicy = args.privacyPolicy
+        org.updatedAt = new Date()
+
+        return org.save()
+    }
+
+    async getMultiple(args) {
+        return Org.find({ orgId: { $in: args.orgId } })
+    }
+
+    async getOrgWithSharedGist(gist) {
+        return Org.find({ gist: gist, sharedGist: true })
+    }
+
+    remove(args) {
+        return Org.findOneAndRemove(selection(args))
+    }
+}
+
+const orgService = new OrgService()
+module.exports = orgService
