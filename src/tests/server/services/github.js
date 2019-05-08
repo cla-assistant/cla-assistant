d598f7998ad129751a0689dfd35ec347d93cc407
Anton Kharitonov <anton.kharitonov@sap.com>
2019-05-08 14:24:21 +0200

    refactoring: replace callback functions with promises in services/github.js

--- src/tests/server/services/github.js
+++ src/tests/server/services/github.js
@@ -1,23 +1,22 @@
-/*global describe, it, beforeEach, afterEach*/
+/*global describe, it, beforeEach*/
 
 // unit test
-let rewire = require('rewire');
-let assert = require('assert');
-let sinon = require('sinon');
+const rewire = require('rewire')
+const assert = require('assert')
+const sinon = require('sinon')
 
 // config
-global.config = require('../../../config');
+global.config = require('../../../config')
 
 // service
-let github = rewire('../../../server/services/github');
-let cache = require('memory-cache');
+const github = rewire('../../../server/services/github')
+const cache = require('memory-cache')
 
-let callStub = sinon.stub();
-let authenticateStub = sinon.stub();
-let getNextPageStub = sinon.stub();
+const callStub = sinon.stub()
+const authenticateStub = sinon.stub()
 
-describe('github:call', function () {
-    let expectedAuth;
+describe('github:call', () => {
+    let expectedAuth
     function OctokitMock(args) {
         assert.deepStrictEqual(args.auth, expectedAuth)
         assert.strictEqual(args.protocol, 'https')
@@ -36,61 +35,56 @@ describe('github:call', function () {
             }
         }
 
-        this.authenticate = authenticateStub;
-
-        this.hasNextPage = function (link) {
-            return link;
-        };
+        this.authenticate = authenticateStub
 
         this.paginate = callStub
     }
 
-    github.__set__('Octokit', OctokitMock);
+    github.__set__('Octokit', OctokitMock)
 
-    beforeEach(function () {
-        github.resetList = {};
-        callStub.reset();
-        authenticateStub.reset();
-        getNextPageStub.reset();
-        cache.clear();
-        expectedAuth = undefined;
-    });
+    beforeEach(() => {
+        github.resetList = {}
+        callStub.reset()
+        cache.clear()
+        expectedAuth = undefined
+        authenticateStub.reset()
+    })
 
-    it('should return an error if obj is not set', async function () {
+    it('should return an error if obj is not set', async () => {
         try {
             await github.call({})
             assert(false, 'Should throw an error')
         } catch (error) {
-            assert.equal(error, 'obj required/obj not found');
+            assert.equal(error.message, 'obj required/obj not found')
         }
-    });
+    })
 
-    it('should return an error if fun is not set', async function () {
+    it('should return an error if fun is not set', async () => {
         try {
             await github.call({ obj: 'obj' })
             assert(false, 'Should throw an error')
         } catch (error) {
-            assert.equal(error, 'fun required/fun not found');
+            assert.equal(error.message, 'fun required/fun not found')
         }
-    });
+    })
 
-    it('should authenticate when token is set', async function () {
-        expectedAuth = 'token token';
-        callStub.resolves({ data: {}, meta: {} });
+    it('should authenticate when token is set', async () => {
+        expectedAuth = 'token token'
+        callStub.resolves({ data: {}, meta: {} })
 
         await github.call({
             obj: 'obj',
             fun: 'fun',
             token: 'token'
-        });
-    });
+        })
+    })
 
-    it('should authenticate when basic authentication is required', async function () {
+    it('should authenticate when basic authentication is required', async () => {
         expectedAuth = {
             username: 'user',
             password: 'pass'
-        };
-        callStub.resolves({ data: {}, meta: {} });
+        }
+        callStub.resolves({ data: {}, meta: {} })
         await github.call({
             obj: 'obj',
             fun: 'fun',
@@ -98,19 +92,19 @@ describe('github:call', function () {
                 user: 'user',
                 pass: 'pass'
             }
-        });
-    });
+        })
+    })
 
-    it('should not authenticate when neither token nor basicAuth are provided', async function () {
-        callStub.resolves({ data: {}, meta: {} });
+    it('should not authenticate when neither token nor basicAuth are provided', async () => {
+        expectedAuth = undefined
+        callStub.resolves({ data: {}, meta: {} })
         await github.call({
             obj: 'obj',
             fun: 'fun'
-        });
-        assert(authenticateStub.notCalled);
-    });
+        })
+    })
 
-    it('should call the appropriate function on the github api', async function () {
+    it('should call the appropriate function on the github api', async () => {
         const testHeaders = {
             link: null,
             'x-oauth-scopes': []
@@ -121,18 +115,18 @@ describe('github:call', function () {
             fun: 'fun'
         })
         assert.deepStrictEqual(res, { data: {}, headers: testHeaders })
-    });
+    })
 
-    it('should return github error', async function () {
-        callStub.rejects('github error');
+    it('should return github error', async () => {
+        callStub.rejects('github error')
         try {
-            const res = await github.call({
+            await github.call({
                 obj: 'obj',
                 fun: 'fun'
             })
             assert(false, 'Should throw an error')
         } catch (error) {
-            assert.equal(error, 'github error');
+            assert.equal(error, 'github error')
         }
-    });
-});
+    })
+})
