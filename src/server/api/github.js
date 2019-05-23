1daebfd32720e8cd09565ecb7bad26be254cdbbe
Anton Kharitonov <anton.kharitonov@sap.com>
2019-05-23 14:00:32 +0200

    small tweaks on api/github.js

--- src/server/api/github.js
+++ src/server/api/github.js
@@ -1,13 +1,11 @@
 // module
-let github = require('../services/github');
-let merge = require('merge');
+const github = require('../services/github')
+const merge = require('merge')
 
 module.exports = {
-    call: async function (req) {
-        const res = await github.call(merge(req.args, {
-            token: req.user.token
-        }))
+    call: async (req) => {
+        const res = await github.call(merge(req.args, { token: req.user.token }))
 
         return { data: res.data, meta: res.headers }
     }
-};
+}
