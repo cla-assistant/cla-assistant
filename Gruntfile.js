f3b634612c83ef67c17d0f65c985ecade407b6fc
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-08-14 15:03:09 +0200

    changed path for coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
                 options: {
                     coverage: true,
                     mask: '**/*.js',
-                    coverageFolder: 'output/coverage'
+                    coverageFolder: 'coverage'
                 }
             }
         },
