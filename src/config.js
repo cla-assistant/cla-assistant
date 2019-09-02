db6f1a10eb1360e19e7729d45ec2c65958ad899b
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-02 14:48:15 +0200

    Slowing down the GitHub Api requests during validateOrgPR to counter the abuse rate limit

--- src/config.js
+++ src/config.js
@@ -36,7 +36,10 @@ module.exports = {
             commit_bots: ['web-flow'],
 
             //delay reaction on webhook
-            enforceDelay: parseInt(process.env.GITHUB_DELAY || '5000', 10)
+            enforceDelay: parseInt(process.env.GITHUB_DELAY || '5000', 10),
+
+            //slow down API calls in order to avoid abuse rate limit
+            timeToWait: process.env.GITHUB_TIME_TO_WAIT || 1000
         },
 
         localport: process.env.PORT || 5000,
