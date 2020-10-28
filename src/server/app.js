c4ecacebda211754bf08414e77739103724a7c01
Fabio Bonelli <fb@fabiobonelli.it>
2020-10-28 14:17:11 +0100

    Set cookie Secure attribute.

--- src/server/app.js
+++ src/server/app.js
@@ -163,6 +163,7 @@ async.series([
             saveUninitialized: true,
             resave: false,
             cookie: {
+                secure: true,
                 maxAge: config.server.security.cookieMaxAge
             },
             store: new MongoStore({
