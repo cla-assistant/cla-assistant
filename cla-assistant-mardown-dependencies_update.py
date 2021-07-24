d5a772953a19d09312ca3dd7d7a8c750754bac28
MoneyMan573 <pimpneasily@yahoo.com>
2021-07-24 18:48:01 -0500

    Update cla-assistant-mardown-dependencies_update.py

--- cla-assistant-mardown-dependencies_update.py
+++ cla-assistant-mardown-dependencies_update.py
@@ -1,1 +1,3 @@
-[![devDependencies Status](https://status.david-dm.org/gh/cla-assistant/cla-assistant.svg?type=dev&style=flat-square)](https://david-dm.org/cla-assistant/cla-assistant?type=dev)
+'-[-!-]-[-devDependencies-Status]-'-']-[-]-[->
+'->'-]-[-(->'status.david-dm.org/gh/cla-assistant/cla-assistant.svg?type=dev&style=flat-square-)-]-[->
+'->'-]-[-(->'david-dm.org/cla-assistant/cla-assistant?type=dev)
