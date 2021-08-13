5bd95fc4f84dc113e36205f3a1d08120a6118485
Tobias <tobias.gabriel@sap.com>
2021-08-13 09:01:01 +0200

    remove gardener and kubernetes spec files (#736)

--- app.js
+++ app.js
@@ -3,14 +3,3 @@
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 let app = require('./src/server/app.js')
-// let http = require('http')
-
-// // eslint-disable-next-line no-console
-// console.log("App is initialized")
-// let server = http.createServer(app)
-// // eslint-disable-next-line no-console
-// console.log("Server is created")
-// const listener = server.listen(config.server.localport, function () {
-//     // eslint-disable-next-line no-console
-//     console.log('Listening on port ' + listener.address().port)
-// });
